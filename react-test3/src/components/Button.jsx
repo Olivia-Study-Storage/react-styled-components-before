46855016809616f6c44a13f65a238ba67d373109
shinheeje <dbfk2637@naver.com>
2023-04-22 15:43:36 +0900

    Feat : NewButtonComponent

--- react-test3/src/components/Button.jsx
+++ react-test3/src/components/Button.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
-
+import styled from 'styled-components'
+import BtnL from './BtnL'
+import BtnM from './BtnM'
+import BtnS from './BtnS'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronRight, faSun } from '@fortawesome/free-solid-svg-icons'
 function Button() {
+
   return (
     <>
-      
+      <h1>Button</h1>
+      <BtnWrap>
+        <BtnL icon1={<FontAwesomeIcon icon={faChevronRight} />} icon2={<FontAwesomeIcon icon={faSun} />} color={'#55efc4'}/>
+        <BtnM color={'#55efc4'}/>
+        <BtnS color={'#55efc4'}/>
+      </BtnWrap>
     </>
   )
 }
+
+const BtnWrap = styled.section`
+    display: flex;
+    gap: 20px;
+`
+
 
 export default Button
